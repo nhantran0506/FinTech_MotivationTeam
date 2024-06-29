c73d1880f1c2b8674064e826d81466878426a043
Nguyen Phuoc Truong <phuoctruong@Admins-MacBook-Pro.local>
2024-06-29 15:21:33 +0700

    update layout

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,9 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import GlobalProvider from "@/context/GlobalProvider";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -27,11 +28,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GlobalProvider>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
+    </GlobalProvider>
   );
 }
