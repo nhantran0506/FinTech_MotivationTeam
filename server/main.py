c942c9b1409a9a7d11afaf865ba4e847876b0fc9
ThanhDuy21110759 <21110759@student.hcmute.edu.vn>
2024-06-29 15:20:21 +0700

    Done QR API

--- server/main.py
+++ server/main.py
@@ -6,6 +6,10 @@ from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from auth import *
 from typing import List
+import qrcode
+import base64
+from io import BytesIO
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -74,5 +78,18 @@ def get_transactions(current_user: models.User = Depends(get_current_user), db :
 def get_balance(current_user: models.User = Depends(get_current_user)):
     return {"balance": current_user.balance}
 
-@app.post("/request_loan")
-def request_loan(curr
+# @app.post("/request_loan")
+# def request_loan(curr
+
+class QRRequest(BaseModel):
+    qr_text: str
+
+@app.post("/qr")
+def generate_qr(qr: QRRequest):
+    qr_image = qrcode.make(qr.qr_text, box_size=15)
+    qr_image_pil = qr_image.get_image()
+    stream = BytesIO()
+    qr_image_pil.save(stream, format='PNG')
+    qr_image_data = stream.getvalue()
+    qr_image_base64 = base64.b64encode(qr_image_data).decode('utf-8')
+    return {"qr_image_base64": qr_image_base64, "variable": qr.qr_text}
